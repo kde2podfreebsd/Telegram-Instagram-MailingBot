f5c24c0b1ea0c51bc31a3fb6e371fe729e6b730c
Ivan Bogomolov <qhomop@gmail.com>
2023-09-10 23:52:47 +0300

    make base struct & U-A json configurator

---
+++ App/UserAgent/Configurator/__init__.py
@@ -0,0 +1,4 @@
+from .ConfiguratorEntities import AppConfig  # noqa
+from .ConfiguratorEntities import ChannelConfig  # noqa
+from .ConfiguratorEntities import ConfigValidationException  # noqa
+from .ConfiguratorEntities import UserAgentConfig  # noqa
---
+++ Makefile
@@ -0,0 +1,9 @@
+pre_commit:
+	 pre-commit run --all-files
+
+pre_commit_flake8:
+	pre-commit run flake8 --all-files
+
+clean:
+	find . -name __pycache__ -type d -print0|xargs -0 rm -r --
+	rm -rf .idea/
