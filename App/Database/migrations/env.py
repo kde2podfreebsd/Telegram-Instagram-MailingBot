d494a51813e686e8805f4d2e0532e7a1598c3922
complicat9d <ivanov.stuff@mail.ru>
2024-04-22 16:57:43 +0300

    update alembic.ini and remove all premade migrations

--- App/Database/migrations/env.py
+++ App/Database/migrations/env.py
@@ -7,7 +7,6 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 from App.Database.Models import Base
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
